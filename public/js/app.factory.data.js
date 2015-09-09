3495c0f0d20f4657fa5f38c5c72c016600faa8ef
Menelik Williams <itsme@menelik.co.uk>
2015-09-09 01:08:33 +0100

    Formatted code

--- public/js/app.factory.data.js
+++ public/js/app.factory.data.js
@@ -5,20 +5,14 @@
     app.factory('DataFcty', ['$http', function($http) {
 
         return {
-
             getJson: function() {
-
-                return $http.get('data.json')
-                    .then(
-                        function(response) {
+                return $http.get('data.json').then(function(response) {
                             return response.data;
                         },
                         function(httpError) {
                             throw httpError.status + " : " + httpError.data;
                         });
-
             }
-
         };
 
     }]);
